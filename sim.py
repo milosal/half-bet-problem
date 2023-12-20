cfb8f07a00654cfc6fe0378881ccb873652503e5
Cole Salvador <colesalvador@college.harvard.edu>
2023-12-20 13:52:48 -0500

    working half-bet

--- sim.py
+++ sim.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 START_BALANCE = 100
+START_BET = 1
 WIN_CHANCE = .5
 PAYOFF_MULTIPLIER = 2
 LOSS_MULTIPLIER = 0.5
-ITERATIONS = 10
+ITERATIONS = 40
 
-def main():
+# Simplest working version of betting sim
+def basic():
     balance = START_BALANCE
     balances = []
     for _ in range(ITERATIONS + 1):
@@ -17,15 +19,37 @@ def main():
         balance = one_iter(balances[-1])
     visualize(balances)
 
-def one_iter(balance):
+def increasing_bet():
+    balance = START_BALANCE
+    balances = []
+    i = START_BET
+    for _ in range(ITERATIONS + 1):
+        balances.append(balance)
+        if balance >= i:
+            amount_at_stake = i
+        elif balance >= START_BET:
+            amount_at_stake = START_BET
+        else:
+            print("Out of money!")
+            break
+        returned_money = one_iter(amount_at_stake)
+        if returned_money > amount_at_stake:
+            i = START_BET
+        else:
+            i *= 2
+        balance += returned_money
+        
+# One win or loss bet with balance resolution
+def one_iter(money):
     result = random.choice([PAYOFF_MULTIPLIER, LOSS_MULTIPLIER])
-    return balance * result
+    return money * result
 
+# Graph of balance over time
 def visualize(balances):
-    x = np.arange(0, ITERATIONS + 1)
+    x = np.arange(0, len(balances))
     y = balances
     plt.plot(x, y)
     plt.show()
 
 if __name__ == "__main__":
-    main()
+    basic()
